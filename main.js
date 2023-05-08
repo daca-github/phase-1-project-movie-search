a8f1056473c7c2781ae5ebfc2b1a3439238c340b
daca-github <danielcornejoaaa@gmail.com>
2023-05-08 19:03:59 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,32 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const searchForm = document.getElementById("search-form")
-    const searchInput = document.getElementById("search-input")
-    const movieContainer = document.getElementById("movie-container")
+    const searchForm = document.getElementById("search-form");
+    const searchInput = document.getElementById("search-input");
+    const moviesContainer = document.getElementById("movie-container");
+
     searchForm.addEventListener("submit", (event) => {
-        event.preventDefault()
-        searchMovies(searchInput.value)
-    })
+        event.preventDefault();
+        searchMovies(searchInput.value);
+    });
 
     function searchMovies(searchTerm) {
-        fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=4f389c01${searchTerm}`)
-        .then((response) => response.json())
-        .then((data) => {
-            if (data.response === "True") {
-                displayMovies(data.Search)
-            } else {
-                moviesContainer.innerhtml = `<p> No Results for "${searchTerm}". </p>`
-            }
-        })
+        fetch(`http://www.omdbapi.com/?apikey=4f389c01&s=${searchTerm}`)
+            .then((response) => response.json())
+            .then((data) => {
+                if (data.Response === "True") {
+                    displayMovies(data.Search);
+                } else {
+                    moviesContainer.innerHTML = `<p>No Results for "${searchTerm}".</p>`;
+                }
+            });
     }
+
     function displayMovies(movies) {
         const movieElements = movies.map((movie) => {
             return `
-            <div class="movie">
-                <h3>${movie.Title}</h3>
-                <p>Year: ${movie.Year}</p>
-            </div>
-            `
-        })
-        
+                <div class="movie">
+                    <h3>${movie.Title}</h3>
+                    <p>Year: ${movie.Year}</p>
+                </div>
+            `;
+        });
+        moviesContainer.innerHTML = movieElements.join("");
     }
-})
+});
